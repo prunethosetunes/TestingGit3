f46d29e9d64c853484f11a1351dbc76e2c483f62
prunethosetunes <86287279+prunethosetunes@users.noreply.github.com>
2021-06-22 04:07:13 -0400

    Add files via upload

---
+++ TestingMerges/app/src/main/java/com/example/testingmerges/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.testingmerges
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val testing = "a"
+        val testing2 = "b"
+        val testing3 = "c"
+        val testing4 = "d"
+    }
+}
